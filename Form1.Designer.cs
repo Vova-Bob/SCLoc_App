8a3bf321d00f016cba154e5d9e6017dbd3965669
Vova-Bob <valdeuscorp@gmail.com>
2024-01-23 16:03:05 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace SCLOCUA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripProgressBar1 = new System.Windows.Forms.ToolStripProgressBar();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel2 = new System.Windows.Forms.LinkLabel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.linkLabel3 = new System.Windows.Forms.LinkLabel();
+            this.statusStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(134)))), ((int)(((byte)(201)))), ((int)(((byte)(248)))));
+            this.label1.Name = "label1";
+            // 
+            // button1
+            // 
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.ForeColor = System.Drawing.SystemColors.InactiveBorder;
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripProgressBar1,
+            this.toolStripStatusLabel1});
+            resources.ApplyResources(this.statusStrip1, "statusStrip1");
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.ManagerRenderMode;
+            // 
+            // toolStripProgressBar1
+            // 
+            this.toolStripProgressBar1.Name = "toolStripProgressBar1";
+            resources.ApplyResources(this.toolStripProgressBar1, "toolStripProgressBar1");
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.BorderSides = System.Windows.Forms.ToolStripStatusLabelBorderSides.Bottom;
+            resources.ApplyResources(this.toolStripStatusLabel1, "toolStripStatusLabel1");
+            this.toolStripStatusLabel1.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.toolStripStatusLabel1.LinkVisited = true;
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            // 
+            // button2
+            // 
+            resources.ApplyResources(this.button2, "button2");
+            this.button2.ForeColor = System.Drawing.Color.Lime;
+            this.button2.Name = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // checkBox1
+            // 
+            resources.ApplyResources(this.checkBox1, "checkBox1");
+            this.checkBox1.ForeColor = System.Drawing.Color.Chartreuse;
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            resources.ApplyResources(this.button3, "button3");
+            this.button3.ForeColor = System.Drawing.Color.Cyan;
+            this.button3.Name = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // checkBox2
+            // 
+            resources.ApplyResources(this.checkBox2, "checkBox2");
+            this.checkBox2.ForeColor = System.Drawing.Color.Chartreuse;
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.UseVisualStyleBackColor = false;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.Color.LightSteelBlue;
+            resources.ApplyResources(this.comboBox1, "comboBox1");
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            resources.GetString("comboBox1.Items"),
+            resources.GetString("comboBox1.Items1"),
+            resources.GetString("comboBox1.Items2"),
+            resources.GetString("comboBox1.Items3"),
+            resources.GetString("comboBox1.Items4"),
+            resources.GetString("comboBox1.Items5")});
+            this.comboBox1.Name = "comboBox1";
+            // 
+            // linkLabel1
+            // 
+            resources.ApplyResources(this.linkLabel1, "linkLabel1");
+            this.linkLabel1.ForeColor = System.Drawing.Color.Coral;
+            this.linkLabel1.LinkBehavior = System.Windows.Forms.LinkBehavior.HoverUnderline;
+            this.linkLabel1.LinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(231)))), ((int)(((byte)(255)))));
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked_1);
+            // 
+            // linkLabel2
+            // 
+            resources.ApplyResources(this.linkLabel2, "linkLabel2");
+            this.linkLabel2.LinkColor = System.Drawing.SystemColors.ActiveCaption;
+            this.linkLabel2.Name = "linkLabel2";
+            this.linkLabel2.TabStop = true;
+            this.linkLabel2.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel2_LinkClicked_1);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            resources.ApplyResources(this.pictureBox1, "pictureBox1");
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.TabStop = false;
+            // 
+            // linkLabel3
+            // 
+            resources.ApplyResources(this.linkLabel3, "linkLabel3");
+            this.linkLabel3.LinkColor = System.Drawing.SystemColors.ControlLightLight;
+            this.linkLabel3.Name = "linkLabel3";
+            this.linkLabel3.TabStop = true;
+            this.linkLabel3.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel3_LinkClicked);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(31)))), ((int)(((byte)(51)))));
+            this.Controls.Add(this.linkLabel3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.linkLabel2);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripProgressBar toolStripProgressBar1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.LinkLabel linkLabel2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.LinkLabel linkLabel3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SCLOCUA
+{
+    public partial class Form1 : Form
+    {
+        private string selectedFolderPath = "";
+        private bool folderDialogShown = false;
+        private int autoUpdateInterval = 1;
+
+        private Timer autoUpdateTimer = new Timer();
+
+        public Form1()
+        {
+            InitializeComponent();
+            button1.Click += button1_Click;
+            LoadSavedFolderPath();
+            this.FormClosing += Form1_FormClosing;
+            checkBox2.CheckedChanged += checkBox2_CheckedChanged;
+            comboBox1.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
+            this.MaximizeBox = false;
+            this.Icon = Properties.Resources.Icon;
+            autoUpdateTimer.Interval = autoUpdateInterval * 60 * 1000;
+            autoUpdateTimer.Tick += autoUpdateTimer_Tick;
+            autoUpdateTimer.Start();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!folderDialogShown)
+            {
+                folderDialogShown = true;
+                using (FolderBrowserDialog folderDialog = new FolderBrowserDialog())
+                {
+                    folderDialog.SelectedPath = selectedFolderPath;
+                    DialogResult result = folderDialog.ShowDialog();
+
+                    if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(folderDialog.SelectedPath))
+                    {
+                        selectedFolderPath = folderDialog.SelectedPath;
+                        SaveSelectedFolderPath();
+                        UpdateLabel();
+                        toolStripStatusLabel1.Text = "Перейдіть до встановлення локалізації";
+                        button2.Enabled = true;
+                    }
+                }
+                folderDialogShown = false;
+            }
+        }
+
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(selectedFolderPath))
+            {
+                toolStripProgressBar1.Maximum = checkBox1.Checked ? 2 : 1;
+                toolStripProgressBar1.Value = 0;
+
+                button2.Enabled = false;
+
+                try
+                {
+                    bool userCfgExists = File.Exists(Path.Combine(selectedFolderPath, "user.cfg"));
+                    bool globalIniExists = File.Exists(Path.Combine(selectedFolderPath, "Data/Localization/korean_(south_korea)/global.ini"));
+                                        
+                    if (checkBox1.Checked)
+                    {
+                        File.Copy("user.cfg", Path.Combine(selectedFolderPath, "user.cfg"), true);
+                        toolStripProgressBar1.Value++;
+                    }
+
+                    await DownloadFileAsync("https://raw.githubusercontent.com/Vova-Bob/SC_localization_UA/main/Data/Localization/korean_(south_korea)/global.ini", Path.Combine(selectedFolderPath, "Data/Localization/korean_(south_korea)/global.ini"));
+                    toolStripProgressBar1.Value++;
+
+                    if (userCfgExists || globalIniExists)
+                    {
+                        toolStripStatusLabel1.Text = "Локалізацію оновлено";
+                    }
+                    else
+                    {
+                        toolStripStatusLabel1.Text = "Локалізацію встановлено";
+                    }
+
+                    autoUpdateTimer.Start();
+                    
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Помилка при завантаженні/оновленні файлів: {ex.Message}", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                finally
+                {
+                    button2.Enabled = true;
+                }
+            }
+        }
+
+        private async Task DownloadFileAsync(string url, string filePath)
+        {
+            using (HttpClient httpClient = new HttpClient())
+            {
+                byte[] fileData = await httpClient.GetByteArrayAsync(url);
+                string directoryPath = Path.GetDirectoryName(filePath);
+
+                if (!Directory.Exists(directoryPath))
+                {
+                    Directory.CreateDirectory(directoryPath);
+                }
+
+                await Task.Run(() => File.WriteAllBytes(filePath, fileData));
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            folderDialogShown = false;
+        }
+
+        private void LoadSavedFolderPath()
+        {
+            if (File.Exists("config.ini"))
+            {
+                selectedFolderPath = File.ReadAllText("config.ini");
+                UpdateLabel();
+                button2.Enabled = !string.IsNullOrWhiteSpace(selectedFolderPath);
+            }
+        }
+
+        private void SaveSelectedFolderPath()
+        {
+            File.WriteAllText("config.ini", selectedFolderPath);
+        }
+
+        private void UpdateLabel()
+        {
+            label1.Text = selectedFolderPath;
+        }
+
+        private async void button3_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(selectedFolderPath))
+            {
+                try
+                {
+                    bool userCfgExists = File.Exists(Path.Combine(selectedFolderPath, "user.cfg"));
+                    bool globalIniExists = File.Exists(Path.Combine(selectedFolderPath, "Data/Localization/korean_(south_korea)/global.ini"));
+
+                    if (userCfgExists || globalIniExists)
+                    {
+                        if (userCfgExists)
+                        {
+                            File.Delete(Path.Combine(selectedFolderPath, "user.cfg"));
+                        }
+                        if (globalIniExists)
+                        {
+                            File.Delete(Path.Combine(selectedFolderPath, "Data/Localization/korean_(south_korea)/global.ini"));
+                        }
+
+                        int initialValue = toolStripProgressBar1.Value;
+
+                        for (int i = initialValue; i >= 0; i--)
+                        {
+                            toolStripProgressBar1.Value = i;
+                            await Task.Delay(50);
+                        }
+
+                        toolStripStatusLabel1.Text = "Файли видалено";
+
+                        button2.Enabled = true;
+                    }
+                    else
+                    {
+                        toolStripStatusLabel1.Text = "Файли вже видалені або відсутні";
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Помилка при видаленні файлів: {ex.Message}", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            autoUpdateTimer.Interval = checkBox2.Checked ? autoUpdateInterval * 60 * 1000 : Int32.MaxValue;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            autoUpdateInterval = Convert.ToInt32(comboBox1.SelectedItem);
+            
+            if (autoUpdateInterval > 0)
+            {
+                autoUpdateTimer.Interval = checkBox2.Checked ? autoUpdateInterval * 60 * 1000 : Int32.MaxValue;
+            }
+        }
+
+        private void autoUpdateTimer_Tick(object sender, EventArgs e)
+        {
+            if (checkBox2.Checked)
+            {
+                button2_Click(sender, e); 
+            }
+        }
+
+        private void linkLabel1_LinkClicked_1(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://crowdin.com/project/star-citizen-localization-ua/invite?h=5459dc1e3bc7eb2319809d529deea2a11952526");
+        }
+
+        private void linkLabel2_LinkClicked_1(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://discord.gg/QVV2G2aKzf");
+        }
+
+        private void linkLabel3_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://github.com/Vova-Bob/SC_localization_UA");
+        }
+    }
+}
