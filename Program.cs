6126067edf00eaefd8e83808d4b2044581f50d89
Vova-Bob <valdeuscorp@gmail.com>
2024-02-07 19:45:22 +0200

    #

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Threading;
+using Microsoft.Win32;
 
 namespace SCLOCUA
 {
@@ -18,8 +19,81 @@ namespace SCLOCUA
             {
                 Application.EnableVisualStyles();
                 Application.SetCompatibleTextRenderingDefault(false);
-                Application.Run(new Form1());
+
+                // Створення форми і об'єкта NotifyIcon
+                Form1 mainForm = new Form1();
+                NotifyIcon notifyIcon = new NotifyIcon();
+                notifyIcon.Icon = mainForm.Icon;
+                notifyIcon.Text = "Українізатор Star Citizen";
+                notifyIcon.DoubleClick += (sender, e) =>
+                {
+                    // Показати форму, коли користувач подвійно клацне на іконці трею
+                    mainForm.Show();
+                    mainForm.WindowState = FormWindowState.Normal;
+                };
+
+                // Додавання контекстного меню для іконки у треї
+                ContextMenu contextMenu = new ContextMenu();
+                MenuItem startupMenuItem = new MenuItem("Запускати при старті");
+                bool isStartupEnabled = IsStartupEnabled();
+                startupMenuItem.Checked = isStartupEnabled;
+                startupMenuItem.Click += (sender, e) =>
+                {
+                    // Зміна стану пункта меню "Запускати при старті"
+                    SetStartup(!isStartupEnabled);
+                    startupMenuItem.Checked = !isStartupEnabled;
+                };
+                contextMenu.MenuItems.Add("Відкрити", (sender, e) =>
+                {
+                    mainForm.Show();
+                    mainForm.WindowState = FormWindowState.Normal;
+                });
+                contextMenu.MenuItems.Add(startupMenuItem);
+                contextMenu.MenuItems.Add("Вихід", (sender, e) =>
+                {
+                    Application.Exit();
+                });
+                notifyIcon.ContextMenu = contextMenu;
+
+                // Додавання обробника події Resize для форми
+                mainForm.Resize += (sender, e) =>
+                {
+                    if (mainForm.WindowState == FormWindowState.Minimized)
+                    {
+                        mainForm.Hide();
+                        notifyIcon.Visible = true; // Показати іконку у треї
+                    }
+                };
+
+                // Запуск програми та відображення головної форми
+                Application.Run(mainForm);
                 mutex.ReleaseMutex();
+            }
+        }
+
+        static string AppName = "SCLocalizationUA";
+        static string RegistryKey = "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run";
+
+        static bool IsStartupEnabled()
+        {
+            using (RegistryKey key = Registry.CurrentUser.OpenSubKey(RegistryKey, true))
+            {
+                return key.GetValue(AppName) != null;
+            }
+        }
+
+        static void SetStartup(bool enable)
+        {
+            using (RegistryKey key = Registry.CurrentUser.OpenSubKey(RegistryKey, true))
+            {
+                if (enable)
+                {
+                    key.SetValue(AppName, Application.ExecutablePath);
+                }
+                else
+                {
+                    key.DeleteValue(AppName, false);
+                }
             }
         }
     }
