c57fc7745c4587bfffc350005fa67edc3a2bd2dc
VALDEUS <valdeuscorp@gmail.com>
2025-08-08 05:57:57 +0300

    Refactor startup mutex and registry checks

---
+++ AppConfig.cs
@@ -0,0 +1,16 @@
+namespace SCLOCUA
+{
+    public static class AppConfig
+    {
+        public const string AppName = "SCLocalizationUA";
+        public const string RegistryKeyPath = "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run";
+        public const string UserCfgFileName = "user.cfg";
+        public const string GlobalIniFileName = "global.ini";
+        public const string LocalizationPath = "Data/Localization/korean_(south_korea)";
+        public const string GithubGistUrlPattern = @"https://gist.github.com/\w+/\w+";
+        public const string GithubReleasesApiUrl = "https://api.github.com/repos/Vova-Bob/SC_localization_UA/releases";
+        public const string LatestGlobalIniUrl = "https://github.com/Vova-Bob/SC_localization_UA/releases/latest/download/global.ini";
+        public const string GitHubApiUrl = "https://api.github.com/repos/Vova-Bob/SCLoc_App/releases/latest";
+        public const string WikiUrl = "https://api.github.com/repos/Vova-Bob/SC_localization_UA/contents/wiki.ini";
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,97 +7,81 @@ namespace SCLOCUA
 {
     static class Program
     {
-        static Mutex mutex = new Mutex(true, "{EA6A248E-8F44-4C82-92F6-03F6A055E637}");
-
         [STAThread]
         static void Main()
         {
-            if (mutex.WaitOne(TimeSpan.Zero, true))
+            using var mutex = new Mutex(true, "{EA6A248E-8F44-4C82-92F6-03F6A055E637}", out bool created);
+            if (!created)
+                return;
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            Form1 mainForm = new Form1();
+            using (NotifyIcon notifyIcon = new NotifyIcon())
             {
-                Application.EnableVisualStyles();
-                Application.SetCompatibleTextRenderingDefault(false);
-
-                // Створення форми і об'єкта NotifyIcon
-                Form1 mainForm = new Form1();
-                using (NotifyIcon notifyIcon = new NotifyIcon())
+                notifyIcon.Icon = mainForm.Icon;
+                notifyIcon.Text = "Українізатор Star Citizen";
+                notifyIcon.DoubleClick += (sender, e) =>
                 {
-                    notifyIcon.Icon = mainForm.Icon;
-                    notifyIcon.Text = "Українізатор Star Citizen";
-                    notifyIcon.DoubleClick += (sender, e) =>
-                    {
-                        // Показати форму, коли користувач подвійно клацне на іконці трею
-                        mainForm.Show();
-                        mainForm.WindowState = FormWindowState.Normal;
-                    };
+                    mainForm.Show();
+                    mainForm.WindowState = FormWindowState.Normal;
+                };
 
-                    // Додавання контекстного меню для іконки у треї
-                    ContextMenu contextMenu = new ContextMenu();
-                    MenuItem startupMenuItem = new MenuItem("Запускати при старті");
-                    bool isStartupEnabled = IsStartupEnabled();
+                ContextMenu contextMenu = new ContextMenu();
+                MenuItem startupMenuItem = new MenuItem("Запускати при старті");
+                bool isStartupEnabled = IsStartupEnabled();
+                startupMenuItem.Checked = isStartupEnabled;
+                startupMenuItem.Click += (sender, e) =>
+                {
+                    isStartupEnabled = !isStartupEnabled;
+                    SetStartup(isStartupEnabled);
                     startupMenuItem.Checked = isStartupEnabled;
-                    startupMenuItem.Click += (sender, e) =>
-                    {
-                        // Зміна стану пункта меню "Запускати при старті"
-                        isStartupEnabled = !isStartupEnabled;
-                        SetStartup(isStartupEnabled);
-                        startupMenuItem.Checked = isStartupEnabled;
-                    };
-                    contextMenu.MenuItems.Add("Відкрити", (sender, e) =>
-                    {
-                        mainForm.Show();
-                        mainForm.WindowState = FormWindowState.Normal;
-                    });
-                    contextMenu.MenuItems.Add(startupMenuItem);
-                    contextMenu.MenuItems.Add("Вихід", (sender, e) =>
-                    {
-                        notifyIcon.Visible = false;
-                        Application.Exit();
-                    });
-                    notifyIcon.ContextMenu = contextMenu;
+                };
+                contextMenu.MenuItems.Add("Відкрити", (sender, e) =>
+                {
+                    mainForm.Show();
+                    mainForm.WindowState = FormWindowState.Normal;
+                });
+                contextMenu.MenuItems.Add(startupMenuItem);
+                contextMenu.MenuItems.Add("Вихід", (sender, e) =>
+                {
+                    notifyIcon.Visible = false;
+                    Application.Exit();
+                });
+                notifyIcon.ContextMenu = contextMenu;
 
-                    Application.ApplicationExit += (s, e) => notifyIcon.Visible = false;
+                Application.ApplicationExit += (s, e) => notifyIcon.Visible = false;
 
-                    // Додавання обробника події Resize для форми
-                    mainForm.Resize += (sender, e) =>
+                mainForm.Resize += (sender, e) =>
+                {
+                    if (mainForm.WindowState == FormWindowState.Minimized)
                     {
-                        if (mainForm.WindowState == FormWindowState.Minimized)
-                        {
-                            mainForm.Hide();
-                            notifyIcon.Visible = true; // Показати іконку у треї
-                        }
-                    };
+                        mainForm.Hide();
+                        notifyIcon.Visible = true;
+                    }
+                };
 
-                    // Запуск програми та відображення головної форми
-                    Application.Run(mainForm);
-                }
-                mutex.ReleaseMutex();
+                Application.Run(mainForm);
             }
+            mutex.ReleaseMutex();
         }
-
-        static string AppName = "SCLocalizationUA";
-        static string RegistryKey = "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run";
 
         static bool IsStartupEnabled()
         {
-            using (RegistryKey key = Registry.CurrentUser.OpenSubKey(RegistryKey, true))
-            {
-                return key.GetValue(AppName) != null;
-            }
+            using RegistryKey key = Registry.CurrentUser.OpenSubKey(AppConfig.RegistryKeyPath, true);
+            return key != null && key.GetValue(AppConfig.AppName) != null;
         }
 
         static void SetStartup(bool enable)
         {
-            using (RegistryKey key = Registry.CurrentUser.OpenSubKey(RegistryKey, true))
-            {
-                if (enable)
-                {
-                    key.SetValue(AppName, Application.ExecutablePath);
-                }
-                else
-                {
-                    key.DeleteValue(AppName, false);
-                }
-            }
+            using RegistryKey key = Registry.CurrentUser.OpenSubKey(AppConfig.RegistryKeyPath, true);
+            if (key == null) return;
+
+            if (enable)
+                key.SetValue(AppConfig.AppName, Application.ExecutablePath);
+            else
+                key.DeleteValue(AppConfig.AppName, false);
         }
     }
 }
