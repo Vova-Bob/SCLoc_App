a0ab00ac43a60577218dececcdb078cb62fda884
VALDEUS <valdeuscorp@gmail.com>
2025-08-08 19:00:44 +0300

    Add hangar timer overlay trigger

--- Form1.cs
+++ Form1.cs
@@ -21,6 +21,7 @@ namespace SCLOCUA
         private ToolTip toolTip = new ToolTip(); // Створення об'єкта ToolTip
         private string selectedFolderPath = "";
         private AntiAFK _antiAFK;
+        private HangarTimerOverlay _hangarOverlay;
 
         public Form1()
         {
@@ -72,6 +73,7 @@ namespace SCLOCUA
             button1.Click += SelectFolderButtonClick;
             button2.Click += UpdateLocalizationButtonClick;
             button3.Click += DeleteFilesButtonClick;
+            buttonExHangar.Click += buttonExHangar_Click;
 
             AssignLink(linkLabel1, "https://docs.google.com/forms/d/e/1FAIpQLSdcNr1EdqUU6K63MVwKyDX7-twxDsCQDw8PfgmDSu_D1q9GRA/viewform");
             AssignLink(linkLabel2, "https://discord.gg/QVV2G2aKzf");
@@ -470,6 +472,19 @@ namespace SCLOCUA
         private void Form1_FormClosing(object sender, FormClosingEventArgs e)
         {
             _antiAFK.Dispose();
+        }
+
+        private void buttonExHangar_Click(object sender, EventArgs e)
+        {
+            if (_hangarOverlay == null || _hangarOverlay.IsDisposed)
+            {
+                _hangarOverlay = new HangarTimerOverlay();
+                _hangarOverlay.Show();
+            }
+            else
+            {
+                _hangarOverlay.BringToFront();
+            }
         }
         // Кнопка KillFeed
         private killFeed overlayForm;
