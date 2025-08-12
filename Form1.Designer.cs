79e3ea825efeeb4936d76f37d4c504adc7aa6913
Vova-Bob <valdeuscorp@gmail.com>
2025-08-12 16:38:34 +0300

    net8-winforms

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,203 +29,200 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.label1 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
-            this.toolStripProgressBar1 = new System.Windows.Forms.ToolStripProgressBar();
-            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
-            this.button2 = new System.Windows.Forms.Button();
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
-            this.button3 = new System.Windows.Forms.Button();
-            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
-            this.linkLabel2 = new System.Windows.Forms.LinkLabel();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.linkLabel3 = new System.Windows.Forms.LinkLabel();
-            this.linkLabel4 = new System.Windows.Forms.LinkLabel();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.linkLabel5 = new System.Windows.Forms.LinkLabel();
-            this.buttonWiki = new System.Windows.Forms.Button();
-            this.buttonClearCache = new System.Windows.Forms.Button();
-            this.buttonAntiAFK = new System.Windows.Forms.Button();
-            this.buttonkillfeed = new System.Windows.Forms.Button();
-            this.statusStrip1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
-            this.SuspendLayout();
+            label1 = new Label();
+            button1 = new Button();
+            statusStrip1 = new StatusStrip();
+            toolStripProgressBar1 = new ToolStripProgressBar();
+            toolStripStatusLabel1 = new ToolStripStatusLabel();
+            button2 = new Button();
+            checkBox1 = new CheckBox();
+            button3 = new Button();
+            linkLabel1 = new LinkLabel();
+            linkLabel2 = new LinkLabel();
+            pictureBox1 = new PictureBox();
+            linkLabel3 = new LinkLabel();
+            linkLabel4 = new LinkLabel();
+            pictureBox2 = new PictureBox();
+            linkLabel5 = new LinkLabel();
+            buttonWiki = new Button();
+            buttonClearCache = new Button();
+            buttonAntiAFK = new Button();
+            buttonkillfeed = new Button();
+            statusStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            SuspendLayout();
             // 
             // label1
             // 
-            resources.ApplyResources(this.label1, "label1");
-            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(134)))), ((int)(((byte)(201)))), ((int)(((byte)(248)))));
-            this.label1.Name = "label1";
+            resources.ApplyResources(label1, "label1");
+            label1.ForeColor = Color.FromArgb(134, 201, 248);
+            label1.Name = "label1";
             // 
             // button1
             // 
-            resources.ApplyResources(this.button1, "button1");
-            this.button1.ForeColor = System.Drawing.SystemColors.InactiveBorder;
-            this.button1.Name = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            resources.ApplyResources(button1, "button1");
+            button1.ForeColor = SystemColors.InactiveBorder;
+            button1.Name = "button1";
+            button1.UseVisualStyleBackColor = true;
             // 
             // statusStrip1
             // 
-            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.toolStripProgressBar1,
-            this.toolStripStatusLabel1});
-            resources.ApplyResources(this.statusStrip1, "statusStrip1");
-            this.statusStrip1.Name = "statusStrip1";
-            this.statusStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.ManagerRenderMode;
+            statusStrip1.Items.AddRange(new ToolStripItem[] { toolStripProgressBar1, toolStripStatusLabel1 });
+            resources.ApplyResources(statusStrip1, "statusStrip1");
+            statusStrip1.Name = "statusStrip1";
+            statusStrip1.RenderMode = ToolStripRenderMode.ManagerRenderMode;
             // 
             // toolStripProgressBar1
             // 
-            this.toolStripProgressBar1.Name = "toolStripProgressBar1";
-            resources.ApplyResources(this.toolStripProgressBar1, "toolStripProgressBar1");
+            toolStripProgressBar1.Name = "toolStripProgressBar1";
+            resources.ApplyResources(toolStripProgressBar1, "toolStripProgressBar1");
             // 
             // toolStripStatusLabel1
             // 
-            this.toolStripStatusLabel1.BorderSides = System.Windows.Forms.ToolStripStatusLabelBorderSides.Bottom;
-            resources.ApplyResources(this.toolStripStatusLabel1, "toolStripStatusLabel1");
-            this.toolStripStatusLabel1.ForeColor = System.Drawing.SystemColors.InfoText;
-            this.toolStripStatusLabel1.LinkVisited = true;
-            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            toolStripStatusLabel1.BorderSides = ToolStripStatusLabelBorderSides.Bottom;
+            resources.ApplyResources(toolStripStatusLabel1, "toolStripStatusLabel1");
+            toolStripStatusLabel1.ForeColor = SystemColors.InfoText;
+            toolStripStatusLabel1.LinkVisited = true;
+            toolStripStatusLabel1.Name = "toolStripStatusLabel1";
             // 
             // button2
             // 
-            resources.ApplyResources(this.button2, "button2");
-            this.button2.ForeColor = System.Drawing.Color.Lime;
-            this.button2.Name = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            resources.ApplyResources(button2, "button2");
+            button2.ForeColor = Color.Lime;
+            button2.Name = "button2";
+            button2.UseVisualStyleBackColor = true;
             // 
             // checkBox1
             // 
-            resources.ApplyResources(this.checkBox1, "checkBox1");
-            this.checkBox1.ForeColor = System.Drawing.Color.Chartreuse;
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.UseVisualStyleBackColor = true;
+            resources.ApplyResources(checkBox1, "checkBox1");
+            checkBox1.ForeColor = Color.Chartreuse;
+            checkBox1.Name = "checkBox1";
+            checkBox1.UseVisualStyleBackColor = true;
             // 
             // button3
             // 
-            resources.ApplyResources(this.button3, "button3");
-            this.button3.ForeColor = System.Drawing.Color.Cyan;
-            this.button3.Name = "button3";
-            this.button3.UseVisualStyleBackColor = true;
+            resources.ApplyResources(button3, "button3");
+            button3.ForeColor = Color.Cyan;
+            button3.Name = "button3";
+            button3.UseVisualStyleBackColor = true;
             // 
             // linkLabel1
             // 
-            resources.ApplyResources(this.linkLabel1, "linkLabel1");
-            this.linkLabel1.ForeColor = System.Drawing.Color.Coral;
-            this.linkLabel1.LinkBehavior = System.Windows.Forms.LinkBehavior.HoverUnderline;
-            this.linkLabel1.LinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(231)))), ((int)(((byte)(255)))));
-            this.linkLabel1.Name = "linkLabel1";
-            this.linkLabel1.TabStop = true;
+            resources.ApplyResources(linkLabel1, "linkLabel1");
+            linkLabel1.ForeColor = Color.Coral;
+            linkLabel1.LinkBehavior = LinkBehavior.HoverUnderline;
+            linkLabel1.LinkColor = Color.FromArgb(0, 231, 255);
+            linkLabel1.Name = "linkLabel1";
+            linkLabel1.TabStop = true;
             // 
             // linkLabel2
             // 
-            resources.ApplyResources(this.linkLabel2, "linkLabel2");
-            this.linkLabel2.LinkColor = System.Drawing.SystemColors.ActiveCaption;
-            this.linkLabel2.Name = "linkLabel2";
-            this.linkLabel2.TabStop = true;
+            resources.ApplyResources(linkLabel2, "linkLabel2");
+            linkLabel2.LinkColor = SystemColors.ActiveCaption;
+            linkLabel2.Name = "linkLabel2";
+            linkLabel2.TabStop = true;
             // 
             // pictureBox1
             // 
-            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
-            resources.ApplyResources(this.pictureBox1, "pictureBox1");
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.TabStop = false;
+            pictureBox1.BackColor = Color.Transparent;
+            resources.ApplyResources(pictureBox1, "pictureBox1");
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.TabStop = false;
             // 
             // linkLabel3
             // 
-            resources.ApplyResources(this.linkLabel3, "linkLabel3");
-            this.linkLabel3.LinkColor = System.Drawing.SystemColors.ControlLightLight;
-            this.linkLabel3.Name = "linkLabel3";
-            this.linkLabel3.TabStop = true;
+            resources.ApplyResources(linkLabel3, "linkLabel3");
+            linkLabel3.LinkColor = SystemColors.ControlLightLight;
+            linkLabel3.Name = "linkLabel3";
+            linkLabel3.TabStop = true;
             // 
             // linkLabel4
             // 
-            resources.ApplyResources(this.linkLabel4, "linkLabel4");
-            this.linkLabel4.LinkColor = System.Drawing.Color.DeepSkyBlue;
-            this.linkLabel4.Name = "linkLabel4";
-            this.linkLabel4.TabStop = true;
+            resources.ApplyResources(linkLabel4, "linkLabel4");
+            linkLabel4.LinkColor = Color.DeepSkyBlue;
+            linkLabel4.Name = "linkLabel4";
+            linkLabel4.TabStop = true;
             // 
             // pictureBox2
             // 
-            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
-            this.pictureBox2.Cursor = System.Windows.Forms.Cursors.WaitCursor;
-            resources.ApplyResources(this.pictureBox2, "pictureBox2");
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.TabStop = false;
-            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            pictureBox2.BackColor = Color.Transparent;
+            pictureBox2.Cursor = Cursors.WaitCursor;
+            resources.ApplyResources(pictureBox2, "pictureBox2");
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.TabStop = false;
+            pictureBox2.Click += pictureBox2_Click;
             // 
             // linkLabel5
             // 
-            resources.ApplyResources(this.linkLabel5, "linkLabel5");
-            this.linkLabel5.LinkColor = System.Drawing.SystemColors.ControlLightLight;
-            this.linkLabel5.Name = "linkLabel5";
-            this.linkLabel5.TabStop = true;
+            resources.ApplyResources(linkLabel5, "linkLabel5");
+            linkLabel5.LinkColor = SystemColors.ControlLightLight;
+            linkLabel5.Name = "linkLabel5";
+            linkLabel5.TabStop = true;
             // 
             // buttonWiki
             // 
-            resources.ApplyResources(this.buttonWiki, "buttonWiki");
-            this.buttonWiki.ForeColor = System.Drawing.Color.MediumSpringGreen;
-            this.buttonWiki.Name = "buttonWiki";
-            this.buttonWiki.UseVisualStyleBackColor = true;
-            this.buttonWiki.Click += new System.EventHandler(this.buttonWiki_Click);
+            resources.ApplyResources(buttonWiki, "buttonWiki");
+            buttonWiki.ForeColor = Color.MediumSpringGreen;
+            buttonWiki.Name = "buttonWiki";
+            buttonWiki.UseVisualStyleBackColor = true;
+            buttonWiki.Click += buttonWiki_Click;
             // 
             // buttonClearCache
             // 
-            resources.ApplyResources(this.buttonClearCache, "buttonClearCache");
-            this.buttonClearCache.ForeColor = System.Drawing.Color.White;
-            this.buttonClearCache.Name = "buttonClearCache";
-            this.buttonClearCache.UseVisualStyleBackColor = true;
-            this.buttonClearCache.Click += new System.EventHandler(this.buttonClearCache_Click);
+            resources.ApplyResources(buttonClearCache, "buttonClearCache");
+            buttonClearCache.ForeColor = Color.White;
+            buttonClearCache.Name = "buttonClearCache";
+            buttonClearCache.UseVisualStyleBackColor = true;
+            buttonClearCache.Click += buttonClearCache_Click;
             // 
             // buttonAntiAFK
             // 
-            resources.ApplyResources(this.buttonAntiAFK, "buttonAntiAFK");
-            this.buttonAntiAFK.ForeColor = System.Drawing.Color.MediumSpringGreen;
-            this.buttonAntiAFK.Name = "buttonAntiAFK";
-            this.buttonAntiAFK.UseVisualStyleBackColor = true;
+            resources.ApplyResources(buttonAntiAFK, "buttonAntiAFK");
+            buttonAntiAFK.ForeColor = Color.MediumSpringGreen;
+            buttonAntiAFK.Name = "buttonAntiAFK";
+            buttonAntiAFK.UseVisualStyleBackColor = true;
             // 
             // buttonkillfeed
             // 
-            resources.ApplyResources(this.buttonkillfeed, "buttonkillfeed");
-            this.buttonkillfeed.ForeColor = System.Drawing.Color.MediumSpringGreen;
-            this.buttonkillfeed.Name = "buttonkillfeed";
-            this.buttonkillfeed.UseVisualStyleBackColor = true;
-            this.buttonkillfeed.Click += new System.EventHandler(this.buttonkillfeed_Click);
+            resources.ApplyResources(buttonkillfeed, "buttonkillfeed");
+            buttonkillfeed.ForeColor = Color.MediumSpringGreen;
+            buttonkillfeed.Name = "buttonkillfeed";
+            buttonkillfeed.UseVisualStyleBackColor = true;
+            buttonkillfeed.Click += buttonkillfeed_Click;
             // 
             // Form1
             // 
             resources.ApplyResources(this, "$this");
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(31)))), ((int)(((byte)(51)))));
-            this.BackgroundImage = global::SCLOCUA.Properties.Resources.launcher2_1;
-            this.Controls.Add(this.buttonkillfeed);
-            this.Controls.Add(this.buttonAntiAFK);
-            this.Controls.Add(this.buttonClearCache);
-            this.Controls.Add(this.buttonWiki);
-            this.Controls.Add(this.linkLabel5);
-            this.Controls.Add(this.pictureBox2);
-            this.Controls.Add(this.linkLabel4);
-            this.Controls.Add(this.linkLabel3);
-            this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.linkLabel2);
-            this.Controls.Add(this.linkLabel1);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.checkBox1);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.statusStrip1);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.label1);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.Name = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
-            this.statusStrip1.ResumeLayout(false);
-            this.statusStrip1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
-            this.ResumeLayout(false);
-            this.PerformLayout();
-
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(0, 31, 51);
+            BackgroundImage = Properties.Resources.launcher2_1;
+            Controls.Add(buttonkillfeed);
+            Controls.Add(buttonAntiAFK);
+            Controls.Add(buttonClearCache);
+            Controls.Add(buttonWiki);
+            Controls.Add(linkLabel5);
+            Controls.Add(pictureBox2);
+            Controls.Add(linkLabel4);
+            Controls.Add(linkLabel3);
+            Controls.Add(pictureBox1);
+            Controls.Add(linkLabel2);
+            Controls.Add(linkLabel1);
+            Controls.Add(button3);
+            Controls.Add(checkBox1);
+            Controls.Add(button2);
+            Controls.Add(statusStrip1);
+            Controls.Add(button1);
+            Controls.Add(label1);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Name = "Form1";
+            Load += Form1_Load;
+            statusStrip1.ResumeLayout(false);
+            statusStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
